ee363bdf017646be151edd5f85555bf67871a0fe
redmserver01-commits <redm.server01@gmail.com>
2026-02-26 13:08:44 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+
+-- ███╗░░░███╗░░░░░██╗██████╗░███████╗██╗░░░██╗
+-- ████╗░████║░░░░░██║██╔══██╗██╔════╝██║░░░██║
+-- ██╔████╔██║░░░░░██║██║░░██║█████╗░░╚██╗░██╔╝
+-- ██║╚██╔╝██║██╗░░██║██║░░██║██╔══╝░░░╚████╔╝░
+-- ██║░╚═╝░██║╚█████╔╝██████╔╝███████╗░░╚██╔╝░░
+-- ╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚══════╝░░░╚═╝░░░
+-- Discord: https://discord.gg/gHRNMDQKzb 
+
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+lua54 'yes'
+
+author 'MJDEV'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
